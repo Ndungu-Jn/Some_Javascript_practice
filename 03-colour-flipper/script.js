f5356aae718d73a233f7df5fd154d3a5ca682935
John Ndungu <ndungulink@gmail.com>
2025-04-11 06:56:17 +0300

    Created Function to randomly select colors to be displayed in the background

--- 03-colour-flipper/script.js
+++ 03-colour-flipper/script.js
@@ -5,3 +5,43 @@ const newColorButton = document.getElementById("new-colour-button");
 const currentColorButton = document.getElementById("current-colour");
 
 //get an array that contains the hexvalues
+const hexValues = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+];
+
+function getRandomHexValues() {
+  const randomIndexPosition = Math.floor(Math.random() * hexValues.length);
+  const randomHexValue = hexValues[randomIndexPosition];
+  return randomHexValue;
+}
+//create another function to get the string
+function getRandomHexString(stringLenght) {
+  let hexString = "";
+  for (let i = 0; i < stringLenght; i++) {
+    hexString += getRandomHexValues();
+  }
+
+  return hexString;
+}
+
+newColorButton.addEventListener("click", () => {
+  const randomHexString = "#" + getRandomHexString(6);
+
+  document.body.style.setProperty("background-color", randomHexString);
+
+  currentColorButton.textContent = randomHexString;
+});
