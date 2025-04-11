e82047cbf5b40c0ebe055604e15da5320e76166a
John Ndungu <ndungulink@gmail.com>
2025-04-11 06:31:00 +0300

    Getting the elemnts by Id

--- 03-colour-flipper/script.js
+++ 03-colour-flipper/script.js
@@ -1,1 +1,7 @@
+//Events for the button.
+//calculate the hexstring and apply it to the background of the page
 
+const newColorButton = document.getElementById("new-colour-button");
+const currentColorButton = document.getElementById("current-colour");
+
+//get an array that contains the hexvalues
