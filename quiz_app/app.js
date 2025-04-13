04b9874c435498d5fed84e13450fd662f5db0208
John Ndungu <ndungulink@gmail.com>
2025-04-13 09:38:50 +0300

    Initialized  quiz-app

---
+++ quiz_app/app.js
@@ -0,0 +1,14 @@
+const questions = [
+  {
+    topic: "Science",
+    questions: "What color is the sky?",
+    possibleAnswers: ["blue", "green", "yellow"],
+    correctAnswer: "blue",
+  },
+  {
+    topic: "tech",
+    questions: "What is  your favourite language to learn?",
+    possibleAnswers: ["javascript", "python", "ruby"],
+    correctAnswer: "javascript",
+  },
+];
